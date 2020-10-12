e198c73d9305992cf4a3b180684b58c066a89a52
ghalox2003 <69943686+ghalox2003@users.noreply.github.com>
2020-10-12 03:34:11 +0200

    Add files via upload

---
+++ Dew it.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace EURO_pupille
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.cursor = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.button1.Location = new System.Drawing.Point(383, 612);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(114, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Choose Picture";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = System.Windows.Forms.AnchorStyles.None;
+            //this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(853, 580);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = true;
+            this.pictureBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pictureBox1_KeyDown);
+            // 
+            // cursor
+            // 
+            this.cursor.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.cursor.Location = new System.Drawing.Point(839, 612);
+            this.cursor.Name = "cursor";
+            this.cursor.Size = new System.Drawing.Size(26, 22);
+            this.cursor.TabIndex = 2;
+            this.cursor.Text = "+";
+            this.cursor.UseVisualStyleBackColor = true;
+            this.cursor.Click += new System.EventHandler(this.cursor_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(25, 612);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 22);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Dew it";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(877, 639);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.cursor);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.pictureBox1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button cursor;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Dew it.cs
@@ -0,0 +1,191 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EURO_pupille
+{
+    public class CentralPixel
+    {
+        public static Point cp1
+        {
+            get;
+            set;
+        }
+        public static Point cp2
+        {
+            get;
+            set;
+        }
+    }
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        OpenFileDialog ofd = new OpenFileDialog();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBox1.Focus();
+            pictureBox1.Select();
+        }
+        int ClickCount = 0;
+        private void pictureBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            Point BasePixel1 = new Point(0, 0);
+            Point BasePixel2 = new Point(0, 0);
+            Graphics g;
+            g = this.CreateGraphics();
+            Pen myPen = new Pen(Color.Blue);
+            myPen.Width = 1;
+            if (e.KeyValue == (Char)Keys.E)
+            {
+                Console.WriteLine(ClickCount);
+                if (ClickCount == 0)
+                {
+                    ClickCount = ClickCount + 1;
+                    BasePixel1 = new Point(MousePosition.X, MousePosition.Y);
+                    Console.WriteLine(BasePixel1);
+                }
+                else if (ClickCount == 1)
+                {
+                    ClickCount = ClickCount + 1;
+                    BasePixel2 = new Point(MousePosition.X, MousePosition.Y);
+                    Console.WriteLine(BasePixel2);
+                    g.DrawLine(myPen, BasePixel1, BasePixel2);
+                    pictureBox1.SendToBack();
+                }
+            }
+            CentralPixel cp = new CentralPixel();
+            CentralPixel.cp1 = BasePixel1;
+            CentralPixel.cp2 = BasePixel2;
+        }
+        string fileGet = "";
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileGet = ofd.FileName;
+                Bitmap bit = new Bitmap(fileGet);
+                pictureBox1.Image = bit;
+
+            }
+        }
+
+        private void cursor_Click(object sender, EventArgs e)
+        {
+            pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            pictureBox1.Size = new System.Drawing.Size(ClientSize.Width, ClientSize.Height);
+            pictureBox1.Location = new System.Drawing.Point(0, 0);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap bit = new Bitmap(fileGet);
+            CentralPixel cp = new CentralPixel();
+            Color black = System.Drawing.Color.Black;
+            Point currentPixel1 = CentralPixel.cp1;
+            Point currentPixel2 = CentralPixel.cp2;
+            Color currentColor1 = bit.GetPixel(CentralPixel.cp1.X, CentralPixel.cp1.Y);
+            Color currentColor2 = bit.GetPixel(CentralPixel.cp2.X, CentralPixel.cp2.Y);
+            int PixelCount = 0;
+            while (currentColor1.R >= 10)                                                           //left
+            {       
+                currentColor1 = bit.GetPixel(currentPixel1.X, currentPixel1.Y);
+                while (bit.GetPixel(currentPixel1.X, (currentPixel1.Y+1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel1.X, currentPixel1.Y, black);
+                    currentPixel1.Y = currentPixel1.Y + 1;
+                }
+                currentPixel1 = CentralPixel.cp2;
+                while (bit.GetPixel(currentPixel1.X, (currentPixel1.Y-1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel1.X, currentPixel1.Y, black);
+                    currentPixel1.Y = currentPixel1.Y - 1;
+                }
+                currentPixel1 = CentralPixel.cp2;
+                currentPixel1.X = currentPixel1.X - 1;
+                PixelCount++;
+                CentralPixel.cp1 = currentPixel1;
+            }
+            currentPixel1 = CentralPixel.cp2;
+            currentPixel1.X = currentPixel1.X + PixelCount;
+            PixelCount = 0;
+            CentralPixel.cp2 = currentPixel1;
+            while (currentColor1.R >= 10)                                                           //right
+            {
+                currentColor1 = bit.GetPixel(currentPixel1.X, currentPixel1.Y);
+                while (bit.GetPixel(currentPixel1.X, (currentPixel1.Y + 1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel1.X, currentPixel1.Y, black);
+                    currentPixel1.Y = currentPixel1.Y + 1;
+                }
+                currentPixel1 = CentralPixel.cp2;
+                while (bit.GetPixel(currentPixel1.X, (currentPixel1.Y - 1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel1.X, currentPixel1.Y, black);
+                    currentPixel1.Y = currentPixel1.Y - 1;
+                }
+                currentPixel1 = CentralPixel.cp2;
+                currentPixel1.X = currentPixel1.X + 1;
+                CentralPixel.cp1 = currentPixel1;
+            }
+            currentPixel1 = CentralPixel.cp2;
+            currentPixel1.X = currentPixel1.X + PixelCount;
+            PixelCount = 0;
+            CentralPixel.cp2 = currentPixel1;
+            while (currentColor2.R >= 10)                                                           //left2
+            {
+                currentColor2 = bit.GetPixel(currentPixel2.X, currentPixel2.Y);
+                while (bit.GetPixel(currentPixel2.X, (currentPixel2.Y + 1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel2.X, currentPixel2.Y, black);
+                    currentPixel2.Y = currentPixel2.Y + 1;
+                }
+                currentPixel2 = CentralPixel.cp2;
+                while (bit.GetPixel(currentPixel2.X, (currentPixel2.Y - 1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel2.X, currentPixel2.Y, black);
+                    currentPixel2.Y = currentPixel2.Y - 1;
+                }
+                currentPixel2 = CentralPixel.cp2;
+                currentPixel2.X = currentPixel2.X + 1;
+                CentralPixel.cp2 = currentPixel2;
+            }
+            currentPixel2 = CentralPixel.cp2;
+            currentPixel2.X = currentPixel2.X + PixelCount;
+            PixelCount = 0;
+            CentralPixel.cp2 = currentPixel2;
+            while (currentColor2.R >= 10)                                                           //right2
+            {
+                currentColor2 = bit.GetPixel(currentPixel2.X, currentPixel2.Y);
+                while (bit.GetPixel(currentPixel2.X, (currentPixel2.Y + 1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel2.X, currentPixel2.Y, black);
+                    currentPixel2.Y = currentPixel2.Y + 1;
+                }
+                currentPixel2 = CentralPixel.cp2;
+                while (bit.GetPixel(currentPixel2.X, (currentPixel2.Y - 1)).R >= 10)
+                {
+                    bit.SetPixel(currentPixel2.X, currentPixel2.Y, black);
+                    currentPixel2.Y = currentPixel2.Y - 1;
+                }
+                currentPixel2 = CentralPixel.cp2;
+                currentPixel2.X = currentPixel2.X + 1;
+                CentralPixel.cp2 = currentPixel2;
+            }
+            currentPixel2 = CentralPixel.cp2;
+            currentPixel2.X = currentPixel2.X + PixelCount;
+            PixelCount = 0;
+            CentralPixel.cp2 = currentPixel2;
+        }
+     
+    }
+}
