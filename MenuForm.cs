0d1f262d8c46738650c897335962e7790bbcc560
ghalox2003 <69943686+ghalox2003@users.noreply.github.com>
2020-10-12 03:34:53 +0200

    Add files via upload

---
+++ MenuForm.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace Indoudou
+{
+    public partial class MenuForm : Form
+    {
+        public MenuForm()
+        {
+            InitializeComponent();
+        }
+
+        EmotionForm EmotionF = new EmotionForm();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            int x = Screen.PrimaryScreen.Bounds.Width;
+            int y = Screen.PrimaryScreen.Bounds.Height;
+            this.Location = new Point(0, 0);
+            this.Size = new Size(x, y);
+        }
+        
+        private void btnMenu_Click(object sender, EventArgs e)
+        {
+            this.Show();
+        }
+
+        private void MinimizeButton_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Minimized;
+        }
+
+        private void CloseButton_Click(object sender, EventArgs e)
+        {
+            while (this.Opacity > 0)
+            {
+                this.Opacity -= 0.025;
+                Thread.Sleep(50);
+            }
+            Application.Exit();
+        }
+
+        private void EmotionsButton_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            EmotionF.Show();
+        }
+    }
+
+}
